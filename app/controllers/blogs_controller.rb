41bcb647fb00b76169a5a4b87fb9e6c1e8f807fe
kztn0112 <ngwn1995@gmail.com>
2021-07-12 14:10:38 +0000

    [Add]@

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,15 +22,15 @@ class BlogsController < ApplicationController
   end
 
   def update
-    blog = Blog.find(params[:id])
-    blog.update(blog_params)
+    @blog = Blog.find(params[:id])
+    @blog.update(blog_params)
     redirect_to blog_path(blog)
   end
 
 
   def destroy
-    blog = Blog.find(params[:id])
-    blog.destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
     redirect_to blogs_path
   end
 
