6428536f3f2a2fa45f58249bd16d19e5bb466c23
kztn0112 <ngwn1995@gmail.com>
2021-09-02 15:00:04 +0000

    [追加]単体テスト

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -11,5 +11,27 @@ RSpec.describe Blog, "投稿モデルに関するテスト", type: :model do
           expect(blog).to be_valid
         end
     end
+
+    context "タイトルが空白の場合" do
+      let(:blog) { build(:blog, title: nil) }
+        it "投稿が保存されるか" do
+          expect(blog).to be_invalid
+        end
+    end
+
+    context "カテゴリが空白の場合" do
+      let(:blog) { build(:blog, category: nil) }
+        it "投稿が保存されるか" do
+          expect(blog).to be_invalid
+        end
+    end
+
+    context "本文が空白の場合" do
+      let(:blog) { build(:blog, body: nil) }
+        it "投稿が保存されるか" do
+          expect(blog).to be_invalid
+        end
+    end
+
   end
 end
